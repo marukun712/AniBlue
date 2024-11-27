ce6f871e947961b59f0511ed20322114db643392
marukun712 <marukun530@gmail.com>
2024-11-27 23:02:55 +0900

    ⚡️ 検索バーを追加

--- app/components/ui/header.tsx
+++ app/components/ui/header.tsx
@@ -1,11 +1,31 @@
+import { FormEvent } from "react";
+import { Input } from "./input";
+import { useNavigate } from "react-router-dom";
+import { Form } from "@remix-run/react";
+
 export function Header() {
+  const navigate = useNavigate();
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const form = new FormData(event.currentTarget);
+    const title = form.get("title");
+
+    if (title) {
+      navigate(`/home/search?title=${title}`);
+    }
+  }
+
   return (
-    <a href="/home">
-      <header className="bg-background border-b border-border">
-        <div className="container mx-auto px-4 py-4 flex justify-between items-center">
+    <header className="bg-background border-b border-border">
+      <div className="container mx-auto px-4 py-4 flex justify-between items-center">
+        <a href="/home">
           <h1 className="text-2xl font-bold text-foreground">AniBlue</h1>
-        </div>
-      </header>
-    </a>
+        </a>
+        <Form onSubmit={handleSubmit} className="w-1/4" method="get">
+          <Input placeholder="アニメ名で検索...." name="title" />
+        </Form>
+      </div>
+    </header>
   );
 }

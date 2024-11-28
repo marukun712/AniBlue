5cf99fecfd297ee248c04766bb96a98bd3b478b4
marukun712 <marukun530@gmail.com>
2024-11-28 09:39:39 +0900

    🐛 BuildTargetの修正

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ declare module "@remix-run/node" {
 }
 
 export default defineConfig({
+  build: {
+    target: ["es2022", "edge89", "firefox89", "chrome89", "safari15"],
+  },
   plugins: [
     remix({
       future: {

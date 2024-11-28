a641b938ede32854f6a36563a3e823f8b7e55ced
marukun712 <marukun530@gmail.com>
2024-11-28 18:31:47 +0900

    🐛 Viteの設定を修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { vercelPreset } from "@vercel/remix/vite";
 
 declare module "@remix-run/node" {
   interface Future {
@@ -21,7 +22,9 @@ export default defineConfig({
         v3_singleFetch: true,
         v3_lazyRouteDiscovery: true,
       },
+      presets: [vercelPreset()],
     }),
+
     tsconfigPaths(),
   ],
 });
